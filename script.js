3883aa3eef7f3d9eb1d1c72bbde18bf792b160f2
Tejaswini Ingle <tejaswiniingle04@gmail.com>
2026-09-19 23:36:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	width
+	height
+	constructor(width,height){
+		this.width = width
+		this.height = height
+	}
 
-class Square extends Animal {}
+	get width(){
+		return this.width
+	}
+
+	get height(){
+		return this.height
+	}
+
+	getArea(width, height){
+		return this.width * this.height
+	}
+}
+
+class Square extends Animal {
+	side
+	constructoe(width,height,side){
+		this.side = side
+		this.width = side
+		this.height = side
+	}
+
+	getPerimeter(side){
+		return 4 * side
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
